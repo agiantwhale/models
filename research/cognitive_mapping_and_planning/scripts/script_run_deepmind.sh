e696547da9a2e3c8559918cc86ba62253ea49a87
Il Jae Lee <iljae@umich.edu>
2018-01-16 16:44:05 -0500

    Added DM launcher script

---
+++ research/cognitive_mapping_and_planning/scripts/script_run_deepmind.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0 PYOPENGL_PLATFORM=egl \
+python scripts/script_nav_agent_release.py --config_name cmp.lmap_Msc.clip5.sbpd_rgb_ST+bench_test \
+                                                         --logdir output/random_maze \
+                                                                  --map training-09x09-0127
