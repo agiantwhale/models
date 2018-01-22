3cc8881a00041ca8abae2e11b84629bed0ba1dfe
Il Jae Lee <iljae@umich.edu>
2018-01-21 21:13:22 -0500

    Added train script, removed CUDA visible devices env var

--- research/cognitive_mapping_and_planning/scripts/script_train_deepmind.sh
+++ research/cognitive_mapping_and_planning/scripts/script_train_deepmind.sh
@@ -1,5 +1,5 @@
 PYOPENGL_PLATFORM=egl \
 PYTHONPATH=".:$PYTHONPATH" \
-python scripts/script_nav_agent_release.py --config_name cmp.lmap_Msc.clip5.sbpd_rgb_ST+bench_test \
+python scripts/script_nav_agent_release.py --config_name cmp.lmap_Msc.clip5.sbpd_rgb_ST+train_train \
                                                          --logdir output/random_maze \
                                                                   --map training-09x09-0127
